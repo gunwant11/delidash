10763b77c7fb31e11adf1690d011b7b8a43a9eb7
Gunwant Wankhede <Gunwant@wankhede.me>
2022-11-19 11:33:24 +0530

    🎨 ui changes

---
+++ screens/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export const HomeScreen = () => {
+  return (
+    <Text className="text-lg text-red-600" >HomeScreen hi</Text>
+  )
+}
